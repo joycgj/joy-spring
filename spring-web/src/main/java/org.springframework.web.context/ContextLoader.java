934ecb20082168a1d518d09871dcd2199b00b633
gaojiechen <gaojiechen@sohu-inc.com>
2013-12-11 13:04:44 +0800

    joy-spring initial.

---
+++ spring-web/src/main/java/org.springframework.web.context/ContextLoader.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2002-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.web.context;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
+import javax.servlet.ServletContext;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.access.BeanFactoryLocator;
+import org.springframework.beans.factory.access.BeanFactoryReference;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextException;
+import org.springframework.context.access.ContextSingletonBeanFactoryLocator;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.ObjectUtils;
+
+/**
+ * Performs the actual initialization work for the root application context.
+ * Called by {@link ContextLoaderListener}.
+ *
+ * <p>Looks for a {@link #CONTEXT_CLASS_PARAM "contextClass"} parameter
+ * at the <code>web.xml</code> context-param level to specify the context
+ * class type, falling back to the default of
+ * {@link org.springframework.web.context.support.XmlWebApplicationContext}
+ * if not found. With the default ContextLoader implementation, any context class
+ * specified needs to implement the ConfigurableWebApplicationContext interface.
+ *
+ * <p>Processes a {@link #CONFIG_LOCATION_PARAM "contextConfigLocation"}
+ * context-param and passes its value to the context instance, parsing it into
+ * potentially multiple file paths which can be separated by any number of
+ * commas and spaces, e.g. "WEB-INF/applicationContext1.xml,
+ * WEB-INF/applicationContext2.xml". Ant-style path patterns are supported as well,
+ * e.g. "WEB-INF/*Context.xml,WEB-INF/spring*.xml" or "WEB-INF/&#42;&#42;/*Context.xml".
+ * If not explicitly specified, the context implementation is supposed to use a
+ * default location (with XmlWebApplicationContext: "/WEB-INF/applicationContext.xml").
+ *
+ * <p>Note: In case of multiple config locations, later bean definitions will
+ * override ones defined in previously loaded files, at least when using one of
+ * Spring's default ApplicationContext implementations. This can be leveraged
+ * to deliberately(故意地) override certain bean definitions via an extra XML file.
+ *
+ * <p>Above and beyond(此外,另外) loading the root application context, this class
+ * can optionally load or obtain and hook up a shared parent context to
+ * the root application context. See the
+ * {@link #loadParentContext(ServletContext)} method for more information.
+ *
+ * @author Juergen Hoeller
+ * @author Colin Sampaleanu
+ * @author Sam Brannen
+ * @since 17.02.2003
+ * @see ContextLoaderListener
+ * @see ConfigurableWebApplicationContext
+ * @see org.springframework.web.context.support.XmlWebApplicationContext
+ */
+public class ContextLoader {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
